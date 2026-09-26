d18d204ca01d3255bb4e1512c15de25995c10d1b
Swadhin <168361244+blueorionn@users.noreply.github.com>
2026-09-26 09:11:29 +0000

    feat: registered all viewsets

--- app/api/catalog/urls.py
+++ app/api/catalog/urls.py
@@ -1,11 +1,20 @@
-from django.urls import path
 from rest_framework import routers
 
-from .views import AlbumViewSet
+from .views import (
+    AlbumViewSet,
+    ArtistViewSet,
+    GenreViewSet,
+    LicenseViewSet,
+    TrackViewSet,
+)
 
 app_name = "catalog"
 
 router = routers.DefaultRouter()
 router.register("albums", AlbumViewSet)
+router.register("artists", ArtistViewSet)
+router.register("genres", GenreViewSet)
+router.register("licenses", LicenseViewSet)
+router.register("tracks", TrackViewSet)
 
 urlpatterns = router.urls
